e7c73a0b5f7b31056e38455cdcfc118701531280
Gautam Tamang <gautam.tamang0000@gmail.com>
2022-01-06 16:59:56 +0545

    features section

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,7 +5,7 @@ import './whatgpt3.css'
 
 const WhatGPT3 = () => {
     return (
-        <div className='gpt3__whatgpt3 section__margin'>
+        <div className='gpt3__whatgpt3 section__margin' id='wgpt3'>
             <div className='gpt3__whatgpt3-feature'>
                 <Feature 
                 title="What is GPT-3" 
